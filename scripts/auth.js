4f623aeedaf81a0dcb48242079264d24359aa83a
ranjanghate1 <rghate@gammastack.com>
2021-07-29 01:18:33 +0530

    user login feature

--- scripts/auth.js
+++ scripts/auth.js
@@ -21,3 +21,19 @@ logout.addEventListener('click', (e) => {
     console.log('User has logged out');
   });
 });
+
+const login = document.querySelector('#login-form');
+
+login.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let email = login['login-email'].value;
+  let password = login['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+  });
+
+  let modal = document.querySelector('#modal-login');
+  M.Modal.getInstance(modal).close();
+  login.reset();
+});
